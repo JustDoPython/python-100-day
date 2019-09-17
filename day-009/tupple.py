d392808ac4961f253d2b18fc44f43cf1659e72f1
beginner-fei <283807775@qq.com>
2019-09-17 15:09:02 +0800

    Python 数据结构-元组
    Python 数据结构-元组

--- day-009/tupple.py
+++ day-009/tupple.py
@@ -2,19 +2,20 @@ tup1 = ('baidu', 'google', 12, 34);
 tup2 = (1, 2, 3, 4, 5 );
 tup3 = "a", "b", "c", "d";
 tup4 = ()
+#print('元组tup1是：%s' % tup1)
+#print('元组tup2是：%s' % tup2)
+#print('元组tup3是：%s' % tup3)
+
 print(tup1)
-print(tup2)
-print(tup3)
 
-print(type(tup3))
-
+print('元组tup3的类型是：%s' % type(tup3))
 
 # 访问元组中元素
 tup1 = ('baidu', 'google',1,2)
 tup2 = (1, 2, 3, 4, 5, 6, 7)
 
-print("tup1[0]: ", tup1[0])
-print("tup2[1:5]: ", tup2[1:5])
+print("tup1中的第一个元素是：", tup1[0])
+print("tup2中的 1-5 范围内的元素是：", tup2[1:5])
 
 # 修改元组
 tup1 = ('baidu', 'google',1,2)
@@ -22,26 +23,26 @@ tup2 = (1, 2, 3, 4, 5, 6, 7)
 tup1 + tup2
 
 # 删除元组
-tup1 = ('baidu', 'google',1,2)
+tup1 = ('baidu', 'google', 1, 2)
 print(tup1)
-del tup1
-print("删除后的元组 tup1 : ")
-print(tup1)
+# del tup1
+# print("删除后的元组 tup1 : ")
+# print(tup1)
 
 
 #元组求长度
-tup1 = ('baidu', 'google',1,2)
-len(tup1)
+tup1 = ('baidu', 'google', 1, 2)
+print('元组 tup1 的长度是：%s' % len(tup1))
 
 # 元组连接
-tup1 = (1,2,3)
-tup2 = (4,5,6)
-tup3 = (7,8,9)
-tup1 + tup2 + tup3
+tup1 = (1, 2, 3)
+tup2 = (4, 5, 6)
+tup3 = (7, 8, 9)
+join_tup = tup1 + tup2 + tup3
 
 # 复制元组
-tup1 = ('abc')
-tup1 * 3
+tup1 = ('复制 abc ')
+print(tup1 * 3)
 
 # 指定元素访问
 content = ('hello','world','!')
@@ -55,15 +56,15 @@ print(content[-2])
 
 # 元组内置函数
 
-tup1 = (21,3,44,56,3,10)
+tup1 = (21, 3, 44, 56, 3, 10)
 # 求个数
-print(len(tup1))
+print('元组 tup1 的长度是：%s' % len(tup1))
 
 # 求最大值
-print(max(tup1))
+print('元组 tup1 的最大值是：%s' % max(tup1))
 
 #　求最小值
-print(min(tup1))
+print('元组 tup1 的最小值是：%s' % min(tup1))
 
 # 元组转换
 # 元组转为list
