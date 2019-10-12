9f144c5114733d604f17a94e8f875061bc812446
acredjb <31613674+acredjb@users.noreply.github.com>
2019-10-12 23:16:23 +0800

    python Scrapy爬虫框架及搭建

---
+++ day-067/items.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import scrapy
+class PeilvItem(scrapy.Item):
+    # define the fields for your item here like:
+    cc  = scrapy.Field()#changci
+    li =  scrapy.Field()#libo
+    b5  = scrapy.Field()#bet365
