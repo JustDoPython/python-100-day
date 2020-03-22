8f1fc10658d2e6f76ba48d33ce387d9fc18d0141
ityard <946444345@qq.com>
2020-03-22 16:42:19 +0800

    用 Python 生成 & 识别二维码

--- qrcode_demo/parseqr_demo.py
+++ qrcode_demo/parseqr_demo.py
@@ -2,5 +2,10 @@ import zxing
 from MyQR import myqr
 
 reader = zxing.BarCodeReader()
-barcode = reader.decode('myqr.gif')
-print(barcode.parsed)
+barcode = reader.decode('gzh.jpg')
+# print(str(barcode.parsed))
+myqr.run(words=str(barcode.parsed),
+         version=1,
+         picture='my.gif',
+         colorized=True,
+	     save_name='gmyqr.gif')
